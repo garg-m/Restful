320d1131cd73575107cdd32c24cbc5d4d242b47a
Mayank garg <gargm1501@gmail.com>
2023-02-26 14:04:30 -0500

    commit

--- rest/src/main/java/com/rest/app/rest/Controller/ApiControllers.java
+++ rest/src/main/java/com/rest/app/rest/Controller/ApiControllers.java
@@ -1,13 +1,27 @@
 package com.rest.app.rest.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rest.app.rest.Controller.Models.User;
+import com.rest.app.rest.Controller.Models.Repo.UserRepo;
+
 @RestController
 public class ApiControllers {
+
+    @Autowired
+    private UserRepo userRepo;
     
     @GetMapping(value = "/")
     public String getPage() {
         return "welcome";
+    }
+
+    @GetMapping(value = "/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
     }
 }
